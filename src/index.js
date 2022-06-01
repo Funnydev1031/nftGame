44d3e5c208df9bcb672e66951458d0d01a3b5e4f
Kiran Nasim <Kiran.cse7@gmail.com>
2022-06-02 02:31:53 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev---utwscq.us.auth0.com"
-      clientId="DpGvdcZFYxn33xajjb4gtliuG0yS6wSK"
+      domain="dev-9zut-zjy.us.auth0.com"
+      clientId="yunn6UVCKAMX4r9yxjsuwakyXZ5GVkaB"
       redirectUri={window.location.origin}
     >
       <App />
